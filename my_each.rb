5a6d7ef0524f20826b0a30f6db91a67750cab82e
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-10-22 17:44:47 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,17 @@
-def my_each # put argument(s) here
+def my_each(collection) # put argument(s) here
   # code here
+  if block_given?
+    i = 0
+    while i < collection.length
+      yield collection[i]
+      i = i + 1
+    end
+    collection
+  else
+    return "No Block!"
+  end
+end
+
+my_each([1,2,3,4]) do |i|
+  i
 end
