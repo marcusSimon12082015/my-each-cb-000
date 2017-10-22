7eedea9077f855a3c70780b869e757069d6d5e48
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-10-22 17:38:44 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,16 @@
-def my_each # put argument(s) here
+def my_each(collection) # put argument(s) here
   # code here
+  if block_given?
+    i = 0
+    while i < collection.length
+      yield collection[i]
+      i = i + 1
+    end
+    collection
+  else
+    puts "No Block!"
+end
+
+my_each([1,2,3,4]) do |i|
+  puts i
 end
